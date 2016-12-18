75aff59fab8c97df945e3dd1c5ed31b9cf88bfec
bisthebis <bism25@hotmail.com>
2016-12-18 20:03:16 +0100

    Process creation

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-var http = require('http')
+const http = require('http')
+const exec = require('child_process').execSync
 
 var server = http.createServer((req, res) => {
-  res.writeHead(200)
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  var content = exec('ls -al')
+  res.write(content + '\n\n')
   res.end('Hello from Raspberry Pi (NodeJS)')
 })
 
