b9b29fc19ba087386de49892ed3fd2e12f6692fb
Boris Martin (aka bisthebis) <bism25@htomail.com>
2016-12-26 17:49:36 +0100

    Led On/Off on GPIO 21

--- main.js
+++ main.js
@@ -7,8 +7,28 @@ const HTTP_PORT = 8080
 var app = express()
 
 app.get('/', (req, res) => {
-  var lines = exec('ls -al').toString().split('\n')
+  var status = exec('/home/pi/Programmation/GPIO/ledSTATUS.sh').toString();
+  if (status[0] == '1') {
+    status = 'Led is ON'
+  } else {
+    status = 'Led is OFF'
+  }
+  var lines = [
+    status
+  ]
   res.render('index.ejs', {contenu: lines})
+}
+)
+
+app.get('/up', (req, res) => {
+  exec('python /home/pi/Programmation/GPIO/ledUP.py')
+  res.redirect('/')
+}
+)
+
+app.get('/down', (req, res) => {
+  exec('python /home/pi/Programmation/GPIO/ledDOWN.py')
+  res.redirect('/')
 }
 )
 
